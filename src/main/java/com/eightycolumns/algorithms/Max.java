9974271cf5f67a292d8ffabd5d70dba42a946b5d
Drew Keller <drew@eightycolumns.com>
2017-08-18 08:10:46 -0400

    Find the largest of three ints

--- src/main/java/com/eightycolumns/algorithms/Max.java
+++ src/main/java/com/eightycolumns/algorithms/Max.java
@@ -1,7 +1,15 @@
 package com.eightycolumns.algorithms;
 
 public class Max {
-  public static int of(int a, int b) {
-    return (a > b) ? a : b;
+  public static int of(int firstInt, int ... subsequentInts) {
+    int max = firstInt;
+
+    for (int i = 0; i < subsequentInts.length; i += 1) {
+      if (subsequentInts[i] > max) {
+        max = subsequentInts[i];
+      }
+    }
+
+    return max;
   }
 }
--- src/test/java/com/eightycolumns/algorithms/MaxTest.java
+++ src/test/java/com/eightycolumns/algorithms/MaxTest.java
@@ -9,4 +9,9 @@ public class MaxTest {
   public void maxFindsLargerOfTwoInts() {
     assertEquals(3, Max.of(2, 3));
   }
+
+  @Test
+  public void maxFindsLargestOfThreeInts() {
+    assertEquals(4, Max.of(2, 3, 4));
+  }
 }
