f0b3ad3fb7f86d310a7b97d5a36a33c9d579ba6a
Drew Keller <drew@eightycolumns.com>
2017-08-18 08:32:02 -0400

    Find the smallest int in an array

--- src/main/java/com/eightycolumns/algorithms/Min.java
+++ src/main/java/com/eightycolumns/algorithms/Min.java
@@ -1,6 +1,18 @@
 package com.eightycolumns.algorithms;
 
 public class Min {
+  public static int in(int[] array) {
+    int min = array[0];
+
+    for (int i = 1; i < array.length; i += 1) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+
+    return min;
+  }
+
   public static int of(int firstInt, int ... subsequentInts) {
     int min = firstInt;
 
--- src/test/java/com/eightycolumns/algorithms/MinTest.java
+++ src/test/java/com/eightycolumns/algorithms/MinTest.java
@@ -19,4 +19,10 @@ public class MinTest {
   public void minFindsSmallestOfThreeInts() {
     assertEquals(2, Min.of(2, 3, 4));
   }
+
+  @Test
+  public void minFindsSmallestIntInArray() {
+    int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    assertEquals(0, Min.in(array));
+  }
 }
