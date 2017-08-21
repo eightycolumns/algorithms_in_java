a29724d5cef7d3548e3650681b7ba9ff68ef3c16
Drew Keller <drew@eightycolumns.com>
2017-08-21 18:13:58 -0400

    Check that number is non-negative

--- src/main/java/com/eightycolumns/algorithms/Fibonacci.java
+++ src/main/java/com/eightycolumns/algorithms/Fibonacci.java
@@ -2,6 +2,12 @@ package com.eightycolumns.algorithms;
 
 public class Fibonacci {
   public static int number(int d) {
+    if (d < 0) {
+      throw new IllegalArgumentException(
+        "Fibonacci.number() expects non-negative integer"
+      );
+    }
+
     return (d < 2) ? d : number(d - 1) + number(d - 2);
   }
 }
--- src/test/java/com/eightycolumns/algorithms/FibonacciTest.java
+++ src/test/java/com/eightycolumns/algorithms/FibonacciTest.java
@@ -1,10 +1,22 @@
 package com.eightycolumns.algorithms;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
 public class FibonacciTest {
+  @Rule
+  public ExpectedException exception = ExpectedException.none();
+
+  @Test
+  public void fibonacciNumberExpectsNonNegativeInteger() {
+    exception.expect(IllegalArgumentException.class);
+    exception.expectMessage("Fibonacci.number() expects non-negative integer");
+    Fibonacci.number(-1);
+  }
+
   @Test
   public void fibonacciNumberZeroIsZero() {
     assertEquals(0, Fibonacci.number(0));
