0290267a8d52abebd5a0ec17202e880083901691
Drew Keller <drew@eightycolumns.com>
2017-08-23 20:25:32 -0400

    Check that integers are non-zero

--- src/main/java/com/eightycolumns/algorithms/LeastCommonMultiple.java
+++ src/main/java/com/eightycolumns/algorithms/LeastCommonMultiple.java
@@ -2,6 +2,12 @@ package com.eightycolumns.algorithms;
 
 public class LeastCommonMultiple {
   public static int of(int a, int b) {
+    if (a == 0 || b == 0) {
+      throw new IllegalArgumentException(
+        "LeastCommonMultiple.of() expects non-zero integers"
+      );
+    }
+
     return Math.abs(a * b / GreatestCommonDivisor.of(a, b));
   }
 }
--- src/test/java/com/eightycolumns/algorithms/LeastCommonMultipleTest.java
+++ src/test/java/com/eightycolumns/algorithms/LeastCommonMultipleTest.java
@@ -1,10 +1,26 @@
 package com.eightycolumns.algorithms;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
 public class LeastCommonMultipleTest {
+  @Rule
+  public ExpectedException exception = ExpectedException.none();
+
+  @Test
+  public void integersMustBeNonZero() {
+    exception.expect(IllegalArgumentException.class);
+
+    exception.expectMessage(
+      "LeastCommonMultiple.of() expects non-zero integers"
+    );
+
+    LeastCommonMultiple.of(0, 1);
+  }
+
   @Test
   public void lcmOf15And35Is105() {
     assertEquals(105, LeastCommonMultiple.of( 15,  35));
